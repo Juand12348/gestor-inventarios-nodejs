78f92b3d11f46e6fdd5780164c3ce6379772c45e
Juan Martinez <juandavidmn0819@gmail.com>
2026-05-26 21:18:38 -0500

    Agregando use cases category archivo de barril

--- src/domain/use-cases/index.ts
+++ src/domain/use-cases/index.ts
@@ -7,3 +7,13 @@ export * from './product/get-all-products.use-case'
 export * from './product/get-product-id.use-case'
 export * from './product/update-product.use-case'
 export * from './product/get-product-name.use-case'
+
+
+// Use cases: Category
+export * from './category/able-category.use-case';
+export * from './category/create-category.use-case';
+export * from './category/disable-category.use-case';
+export * from './category/get-all-categories.use-case';
+export * from './category/get-category-id.use-case';
+export * from './category/get-category-name.use-case';
+export * from './category/update-category.use-case';

58e9ffab6ff5b95ab772043383e5f816935349d8
Juan Martinez <juandavidmn0819@gmail.com>
2026-06-01 17:33:43 -0500

    Agregando product datasource impl

--- src/domain/entities/product.entity.ts
+++ src/domain/entities/product.entity.ts
@@ -157,6 +157,9 @@ export class ProductEntity {
     }   
 
     
+    
+
+    
 
     
     
---
+++ src/infrastructure/datasources/product/product.datasource.impl.ts
@@ -0,0 +1,105 @@
+import { AppDataSource } from "../../../data/data-source";
+import { ProductModel } from "../../../data/models/product.model";
+import {
+    CustomError,
+    ProductDatasource,
+    ProductEntity,
+} from "../../../domain";
+
+export class ProductDatasourceImpl implements ProductDatasource {
+
+    private readonly repository =
+        AppDataSource.getRepository(ProductModel);
+
+    async getById(id: string): Promise<ProductEntity> {
+
+        const product = await this.repository.findOne({
+            where: { id },
+            relations: {
+                category: true,
+            },
+        });
+
+        if (!product) {
+            throw CustomError.notFound('Product not exists');
+        }
+
+        return ProductEntity.fromObject(product);
+    }
+
+    async getAll(): Promise<ProductEntity[]> {
+
+        const products = await this.repository.find({
+            relations: {
+                category: true,
+            },
+        });
+
+        return products.map(product =>
+            ProductEntity.fromObject(product)
+        );
+    }
+
+    async getName(name: string): Promise<ProductEntity> {
+
+        const product = await this.repository.findOne({
+            where: { name },
+            relations: {
+                category: true,
+            },
+        });
+
+        if (!product) {
+            throw CustomError.notFound('Product not exists');
+        }
+
+        return ProductEntity.fromObject(product);
+    }
+
+    async create(product: ProductEntity): Promise<ProductEntity> {
+
+        const model = this.repository.create({
+            id: product.idValue,
+            name: product.nameValue,
+            description: product.descriptionValue,
+            price: product.priceValue,
+            stock: product.stockValue,
+            available: product.availableValue,
+            category: {
+                id: product.categoryIdValue,
+            } as any,
+        });
+
+        const saved = await this.repository.save(model);
+
+        return ProductEntity.fromObject(saved);
+    }
+
+    async update(
+        id: string,
+        product: ProductEntity
+    ): Promise<ProductEntity> {
+
+        const model = await this.repository.findOne({
+            where: { id },
+        });
+
+        if (!model) {
+            throw CustomError.notFound('Product not exists');
+        }
+
+        model.name = product.nameValue;
+        model.description = product.descriptionValue;
+        model.price = product.priceValue;
+        model.stock = product.stockValue;
+        model.available = product.availableValue;
+
+        model.category = {
+            id: product.categoryIdValue,
+        } as any;
+
+        const updated = await this.repository.save(model);
+
+        return ProductEntity.fromObject(updated);
+    }
+}
